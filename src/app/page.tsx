6d072a49d1a17f650cf2bb82541132ec4e4d5350
JustAdamCain <03adamcain@gmail.com>
2024-01-23 21:24:30 +0000

    Add login link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
       <h1 className="text-white">
         Home Page
+        <Link href="/login">Login</Link>
       </h1>
     </div>
   );
