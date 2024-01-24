1d76ec9f38cb5bd126c2eab8df44d6894e4f58ce
JustAdamCain <03adamcain@gmail.com>
2024-01-24 02:18:12 +0000

    Add remotePatterns for image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: {
-          allowedOrigins: ["app.localhost:3000"],
-        },
-      }
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["app.localhost:3000"],
+    },
+  },
+  images: {
+    remotePatterns: [
+      { hostname: "public.blob.vercel-storage.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "abs.twimg.com" },
+      { hostname: "pbs.twimg.com" },
+      { hostname: "avatar.vercel.sh" },
+      { hostname: "avatars.githubusercontent.com" },
+      { hostname: "www.google.com" },
+      { hostname: "flag.vercel.app" },
+      { hostname: "illustrations.popsy.co" },
+    ]
+  },
 };
 
 export default nextConfig;
