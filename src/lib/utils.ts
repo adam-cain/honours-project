930186dc31cc86b7716f86bdc72c074761967530
JustAdamCain <03adamcain@gmail.com>
2024-01-24 00:50:42 +0000

    Add next-mdx-remote package and utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,37 @@ import { clsx, type ClassValue } from "clsx";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export async function fetcher<JSON = any>(
+    input: RequestInfo,
+    init?: RequestInit,
+): Promise<JSON> {
+    const response = await fetch(input, { ...init, cache: "no-store" });
+
+    return response.json();
+}
+
+export const capitalize = (s: string) => {
+    if (typeof s !== "string") return "";
+    return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+export const truncate = (str: string, num: number) => {
+    if (!str) return "";
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + "...";
+  };
+
+export const toDateString = (date: Date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+    });
+};
+
+export const random = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};  
